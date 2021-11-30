f98405dd4fb87220d8f70ad83b8010c5cbff8b90
Jesús Jiménez <jesuswebdev@gmail.com>
2021-11-30 12:11:16 -0400

    initial commit

---
+++ common/src/index.ts
@@ -0,0 +1,21 @@
+function invalidNumber(v: number) {
+  return (
+    typeof v === 'undefined' ||
+    v === null ||
+    v === Infinity ||
+    v === -Infinity ||
+    isNaN(v)
+  );
+}
+
+/**
+ *
+ * @description Replaces NaN values with zeros (or given value).
+ * @param v value
+ * @param d value to use if `v` is not valid
+ */
+function nz(v: number, d: number): number {
+  return invalidNumber(v) ? d ?? 0 : v;
+}
+
+export { nz };
