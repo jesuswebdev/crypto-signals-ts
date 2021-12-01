58aec23e661b6c646a1d77d243f72b38f4138a74
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-01 12:43:49 -0400

    add tests

---
+++ common/test/index.test.ts
@@ -0,0 +1,59 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import { nz } from '../src/index';
+
+function getNullValue(): unknown {
+  return null;
+}
+
+function getUndefinedValue(): unknown {
+  return undefined;
+}
+
+describe('Index file functions tests', function () {
+  describe('nz() tests', function () {
+    it('returns zero when value is Infinity', function () {
+      const value = Infinity;
+      const result = nz(value);
+      expect(result).to.eq(0);
+    });
+
+    it('returns zero when value is -Infinity', function () {
+      const value = -Infinity;
+      const result = nz(value);
+      expect(result).to.eq(0);
+    });
+
+    it('returns zero when value is NaN', function () {
+      const value = NaN;
+      const result = nz(value);
+      expect(result).to.eq(0);
+    });
+
+    it('returns zero when value is undefined', function () {
+      const value = getUndefinedValue();
+      const result = nz(value as number);
+      expect(result).to.eq(0);
+    });
+
+    it('returns zero when value is null', function () {
+      const value = getNullValue();
+      const result = nz(value as number);
+      expect(result).to.eq(0);
+    });
+
+    it('returns the supplied number when value is invalid', function () {
+      const value = NaN;
+      const supplied = 1337;
+      const result = nz(value, supplied);
+      expect(result).to.eq(supplied);
+    });
+
+    it('returns the value if it is valid', function () {
+      const value = 1;
+      const result = nz(value);
+      expect(result).to.eq(value);
+    });
+  });
+});
