e40c72e4266426a98abe9122de48dae102294076
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-21 01:28:33 -0400

    export mongoose plugin and message broker plugin interfaces

--- common/src/interfaces/index.ts
+++ common/src/interfaces/index.ts
@@ -1,2 +1,10 @@
+import { Connection } from 'mongoose';
 export * from './candle';
 export * from './market';
+
+export interface MessageBrokerPlugin {
+  publish: <T>(topic: string, data: T) => void;
+}
+export interface MongoosePlugin {
+  connection: Connection;
+}
