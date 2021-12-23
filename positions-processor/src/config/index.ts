b58de383f19cd0634941d6a433fc798128c347b3
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-23 15:51:19 -0400

    validate env variables

--- positions-processor/src/config/index.ts
+++ positions-processor/src/config/index.ts
@@ -1,5 +1,18 @@
+import { validateObjectSchema } from '@jwd-crypto-signals/common';
+import joi from 'joi';
 import dotenv from 'dotenv';
 dotenv.config();
+
+validateObjectSchema(
+  process.env,
+  joi.object({
+    MONGODB_SERVICE_HOST: joi.string().trim().hostname().required(),
+    MONGODB_SERVICE_PORT: joi.number().port().default(27017),
+    MONGODB_DATABASE: joi.string().trim().required(),
+    RABBITMQ_SERVICE_HOST: joi.string().trim().hostname().required(),
+    RABBITMQ_SERVICE_PORT: joi.number().port().default(5672)
+  })
+);
 
 export const MONGODB_URI =
   `mongodb://${process.env.MONGODB_SERVICE_HOST}:${process.env.MONGODB_SERVICE_PORT}/${process.env.MONGODB_DATABASE}` ??
