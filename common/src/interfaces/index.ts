1f013b82d5642f93a35c931a73efd6fb2583bf97
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-22 21:48:35 -0400

    export models and interfaces

--- common/src/interfaces/index.ts
+++ common/src/interfaces/index.ts
@@ -2,6 +2,8 @@ import { Connection } from 'mongoose';
 import { RedisClientType } from 'redis';
 export * from './candle';
 export * from './market';
+export * from './signal';
+export * from './position';
 
 export interface MessageBrokerPlugin {
   publish: <T>(topic: string, data: T) => void;
--- common/src/models/index.ts
+++ common/src/models/index.ts
@@ -1,2 +1,4 @@
 export * from './candle';
 export * from './market';
+export * from './position';
+export * from './signal';
