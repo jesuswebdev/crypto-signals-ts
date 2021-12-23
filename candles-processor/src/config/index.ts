39aee2b5d3ef2c4af04d2939ac6986856bd1b464
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-23 15:39:21 -0400

    validate env variables

--- candles-processor/src/config/index.ts
+++ candles-processor/src/config/index.ts
@@ -1,6 +1,28 @@
-import { MILLISECONDS } from '@jwd-crypto-signals/common';
+import { MILLISECONDS, validateObjectSchema } from '@jwd-crypto-signals/common';
+import joi from 'joi';
 import dotenv from 'dotenv';
 dotenv.config();
+
+validateObjectSchema(
+  process.env,
+  joi.object({
+    MONGODB_SERVICE_HOST: joi.string().trim().hostname().required(),
+    MONGODB_SERVICE_PORT: joi.number().port().default(27017),
+    MONGODB_DATABASE: joi.string().trim().required(),
+    REDIS_SERVICE_HOST: joi.string().trim().hostname().required(),
+    REDIS_SERVICE_PORT: joi.number().port().default(6379),
+    RABBITMQ_SERVICE_HOST: joi.string().trim().hostname().required(),
+    RABBITMQ_SERVICE_PORT: joi.number().port().default(5672),
+    PROCESS_CANDLES_INTERVAL: joi.number().integer().positive().required(),
+    CANDLE_INTERVAL: joi
+      .string()
+      .pattern(new RegExp('^[\\d]{1,2}(d|h|m)$'))
+      .required(),
+    BINANCE_API_URL: joi.string().trim().uri().required(),
+    BINANCE_API_KEY: joi.string().trim().base64().required(),
+    BINANCE_API_SECRET: joi.string().trim().base64().required()
+  })
+);
 
 export const MONGODB_URI =
   `mongodb://${process.env.MONGODB_SERVICE_HOST}:${process.env.MONGODB_SERVICE_PORT}/${process.env.MONGODB_DATABASE}` ??
