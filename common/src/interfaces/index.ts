cc2592ca247c1dec0df1a84aa39ccd4e6b8a1280
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-23 13:14:45 -0400

    add publish options parameter to broker plugin interface

--- common/src/interfaces/index.ts
+++ common/src/interfaces/index.ts
@@ -1,12 +1,17 @@
 import { Connection } from 'mongoose';
 import { RedisClientType } from 'redis';
+import amqplib from 'amqplib';
 export * from './candle';
 export * from './market';
 export * from './signal';
 export * from './position';
 
 export interface MessageBrokerPlugin {
-  publish: <T>(topic: string, data: T) => void;
+  publish: <T>(
+    topic: string,
+    data: T,
+    options?: amqplib.Options.Publish
+  ) => void;
 }
 export interface MongoosePlugin {
   connection: Connection;
